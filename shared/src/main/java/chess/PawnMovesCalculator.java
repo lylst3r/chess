66abb638d07a07a199c338711dbd98eac85fa757
Lily Nahoopii <lylnaho@gmail.com>
2025-09-19 00:27:17 -0600

    impl(p0) pawn moves calculator different team colors, first turn, and capturing

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -17,19 +17,82 @@ public class PawnMovesCalculator {
 
 
         //promotion
-        if (i+1 == 8) {
+        /*if (i+1 == 8) {
             moves = addMovePawn(i, j, 1, 0, moves, board, true); //up
             return moves;
-        }
+        }*/
 
         //normal move
-        moves = addMovePawn(i, j, 1, 0, moves, board, false); //up
+        if (board.getPiece(new ChessPosition(i, j)).getTeamColor() == ChessGame.TeamColor.WHITE) {
+            moves = addMovePawn(i, j, 1, 0, moves, board, false); //up
+        }
+        else if (board.getPiece(new ChessPosition(i, j)).getTeamColor() == ChessGame.TeamColor.BLACK) {
+            moves = addMovePawn(i, j, -1, 0, moves, board, false); //up
+        }
+
 
         return moves;
     }
 
     public static ArrayList<ChessMove> addMovePawn(int r, int c, int dr, int dc, ArrayList<ChessMove> m, ChessBoard board, boolean promotion) {
-        int startRow = r;
+        if (board.getPiece(new ChessPosition(r, c)).getTeamColor() == ChessGame.TeamColor.WHITE) {
+            int startRow = r;
+            int startCol = c;
+            if (r+dr >= 1 && r+dr <= 8 && c+dc >= 1 && c+dc <= 8) {
+
+                //if diagonals occupied by the opposite team move is allowed
+                if (r+1 <= 8 && c-1 >= 1 && c+1 <= 8) {
+                    if (checkSpot(startRow, startCol, r+1, c-1, board) == 2) {
+                        m.add(new ChessMove(new ChessPosition(startRow, startCol), new ChessPosition(r+1, c-1), null));
+                    }
+                    if (checkSpot(startRow, startCol, r+1, c+1, board) == 2) {
+                        m.add(new ChessMove(new ChessPosition(startRow, startCol), new ChessPosition(r+1, c+1), null));
+                    }
+                }
+
+                //normal move option
+                r = startRow + dr;
+                c = startCol + dc;
+                if (checkSpot(startRow, startCol, r, c, board) != 0) {
+                    return m;
+                }
+                m.add(new ChessMove(new ChessPosition(startRow, startCol), new ChessPosition(r, c), null));
+
+                if (startRow == 2 && checkSpot(startRow, startCol, r+1, c, board) == 0) {
+                    m.add(new ChessMove(new ChessPosition(startRow, startCol), new ChessPosition(r+1, c), null));
+                }
+            }
+        }
+        else if (board.getPiece(new ChessPosition(r, c)).getTeamColor() == ChessGame.TeamColor.BLACK) {
+            int startRow = r;
+            int startCol = c;
+            if (r+dr >= 1 && r+dr <= 8 && c+dc >= 1 && c+dc <= 8) {
+
+                //if diagonals occupied by the opposite team move is allowed
+                if (r-1 >= 1 && c-1 >= 1 && c+1 <= 8) {
+                    if (checkSpot(startRow, startCol, r-1, c-1, board) == 2) {
+                        m.add(new ChessMove(new ChessPosition(startRow, startCol), new ChessPosition(r-1, c-1), null));
+                    }
+                    if (checkSpot(startRow, startCol, r-1, c+1, board) == 2) {
+                        m.add(new ChessMove(new ChessPosition(startRow, startCol), new ChessPosition(r-1, c+1), null));
+                    }
+                }
+
+                //normal move option
+                r = startRow + dr;
+                c = startCol + dc;
+                if (checkSpot(startRow, startCol, r, c, board) != 0) {
+                    return m;
+                }
+                m.add(new ChessMove(new ChessPosition(startRow, startCol), new ChessPosition(r, c), null));
+
+                if (startRow == 7 && checkSpot(startRow, startCol, r-1, c, board) == 0) {
+                    m.add(new ChessMove(new ChessPosition(startRow, startCol), new ChessPosition(r-1, c), null));
+                }
+            }
+        }
+
+        /*int startRow = r;
         int startCol = c;
         if (r+dr >= 1 && r+dr <= 8 && c+dc >= 1 && c+dc <= 8) {
 
@@ -52,7 +115,7 @@ public class PawnMovesCalculator {
             if (startRow == 2 && checkSpot(startRow, startCol, r+1, c, board) == 0) {
                 m.add(new ChessMove(new ChessPosition(startRow, startCol), new ChessPosition(r+1, c), null));
             }
-        }
+        }*/
         return m;
     }
 
