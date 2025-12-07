7df24b4febc7a969f5f86690246389573b04e90c
Lily Nahoopii <lylnaho@gmail.com>
2025-12-06 18:38:14 -0700

    impl(p6)command and message subclasses

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,4 +1,38 @@
 package server.websocket;
 
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ConnectionManager {
+
+    private final Map<Integer, Set<Session>> gameConnections = new ConcurrentHashMap<>();
+
+    public void add(int gameID, Session session) {
+        gameConnections.putIfAbsent(gameID, ConcurrentHashMap.newKeySet());
+        gameConnections.get(gameID).add(session);
+    }
+
+    public void remove(Session session) {
+        for (var set : gameConnections.values()) {
+            set.remove(session);
+        }
+    }
+
+    public void broadcast(int gameID, Session exclude, ServerMessage message) throws IOException {
+        var sessions = gameConnections.get(gameID);
+        if (sessions == null) return;
+
+        String json = new Gson().toJson(message);
+
+        for (Session s : sessions) {
+            if (s.isOpen() && !s.equals(exclude)) {
+                s.getRemote().sendString(json);
+            }
+        }
+    }
 }
